0324c4ea9bc29b5154d5bb7569b70d5b8341339b
VikramSGIT <vikram16saminathan@gmail.com>
2021-02-01 12:52:17 -0800

    Added extra extras

--- session_3/blogspot/app/controllers/application_controller.rb
+++ session_3/blogspot/app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user
+    #This protect from cancancan assertion and prints the acces violation
+    #error on index of session
+    protect_from_forgery
+      rescue_from CanCan::AccessDenied do |exception|
+        flash[:notice] = exception.message
+        redirect_to root_url
+      end
   
     def current_user
       if session[:user_id]
